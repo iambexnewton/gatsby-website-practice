ac9c5b3dac35d5e697063af05dfe5bccb044b3ad
Becky Newton <iambexnewton@gmail.com>
2021-10-30 19:28:22 +0200

    banner image added

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Layout from "../../components/Layout"
+import { portfolio } from "/Users/beckynewton/Desktop/gatsby/practice-website/src/styles/projects.module.css"
 
 export default function Projects() {
   return (
     <Layout>
-      <div>
+      <div className={portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've created</h3>
       </div>
