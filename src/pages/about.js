66716cdd321513fe24b67ef9bd41abe53d704646
Becky Newton <iambexnewton@gmail.com>
2021-10-30 15:06:06 +0200

    basic pages created

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Lorem ipsum</p>
+      <p>Lorem ipsum</p>
+      <p>Lorem ipsum</p>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <div>
+        <h2>Design</h2>
+        <h3>Develop & Depoly</h3>
+        <p>UX designer & Web developer based in Denmark</p>
+      </div>
+    </section>
+  )
 }
