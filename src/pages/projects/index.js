bcb4fb68646543458ffb04f0152ec3ac3c178f62
Becky Newton <iambexnewton@gmail.com>
2021-11-01 12:47:24 +0100

    projects displaying in date order complete with css

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,46 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
-import { portfolio } from "/Users/beckynewton/Desktop/gatsby/practice-website/src/styles/projects.module.css"
+import { portfolio, projectstyle } from "../../styles/projects.module.css"
 
-export default function Projects() {
+export default function Projects({ data }) {
+  console.log(data)
+  const projects = data.allMarkdownRemark.nodes
+
   return (
     <Layout>
       <div className={portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've created</h3>
+        <div className={projectstyle}>
+          {projects.map(project => (
+            <Link to={"/projects" + project.frontmatter.slug} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ProjectsPage {
+    allMarkdownRemark(
+      filter: {}
+      sort: { fields: frontmatter___date, order: DESC }
+    ) {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+  }
+`
