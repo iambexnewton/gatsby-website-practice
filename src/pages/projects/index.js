5415ea4bc27d7d1dd50084d8ce3e057ade8c4638
Becky Newton <iambexnewton@gmail.com>
2021-11-02 19:07:38 +0100

    rendering thumbnails onto projects

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,6 +2,7 @@ import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
 import { portfolio, projectstyle } from "../../styles/projects.module.css"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 export default function Projects({ data }) {
   console.log(data)
@@ -17,6 +18,10 @@ export default function Projects({ data }) {
           {projects.map(project => (
             <Link to={"/projects" + project.frontmatter.slug} key={project.id}>
               <div>
+                <GatsbyImage
+                  image={getImage(project.frontmatter.thumb)}
+                  alt="project image"
+                />
                 <h3>{project.frontmatter.title}</h3>
                 <p>{project.frontmatter.stack}</p>
               </div>
@@ -40,6 +45,14 @@ export const query = graphql`
           slug
           stack
           title
+          thumb {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+          slug
+          title
+          stack
         }
         id
       }
