1adf53db190129eecd9e6bd1545cd6862c458d4c
Becky Newton <iambexnewton@gmail.com>
2021-11-04 13:18:42 +0100

    css applied

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,9 +6,27 @@ export default function About() {
     <Layout>
       <div>
         <h1>About Page</h1>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
+        <p>This is a little project for me to learn the basics of Gatsby.</p>
+        <h2>What is Gatsby?</h2>
+        <p>
+          Gatsby is an open source framework which enables developers to build
+          fast, secure websites using a React based framework
+        </p>
+        <h2>What did I learn during the project?</h2>
+        <p>
+          In the project I used dynamic data and static metadata. Which was
+          rendered by using Gatsby plugins
+        </p>
+        <p>
+          During the project I learned about queries, template components,
+          markdown files and how to render the information dynamically.
+        </p>
+        <div>
+          <em>
+            *This project uses images provided from an out of date Gatsby
+            tutorial.
+          </em>
+        </div>
       </div>
     </Layout>
   )
--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,22 +1,48 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout"
-// import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { details, featured } from "../styles/projects-details.module.css"
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+  const { html } = data.markdownRemark
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter
+  console.log(data)
+
   return (
     <Layout>
       <div className={details}>
-        <h2>title</h2>
-        <h3>stack</h3>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
         <div className={featured}>
-          {/* <GatsbyImage /> */}
-          {/* image={getImage(project.frontmatter.thumb)}
-                  alt="project image" */}
+          <GatsbyImage
+            image={getImage(featuredImg.childImageSharp.gatsbyImageData)}
+            alt="project image"
+          />
         </div>
-        {/* <div className={html} dangerouslySetInnerHTML={} /> */}
+        <div className={html} dangerouslySetInnerHTML={{ __html: html }} />
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            gatsbyImageData(
+              layout: FULL_WIDTH
+              placeholder: BLURRED
+              formats: [AUTO, AVIF]
+            )
+          }
+        }
+      }
+    }
+  }
+`
