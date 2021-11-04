bcac40de9afecf4f8625df928f9a451901df8514
Becky Newton <iambexnewton@gmail.com>
2021-11-04 19:13:59 +0100

    ready to start deploy

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,23 +10,22 @@ export default function About() {
         <h2>What is Gatsby?</h2>
         <p>
           Gatsby is an open source framework which enables developers to build
-          fast, secure websites using a React based framework
+          fast, secure websites using a React based framework.
         </p>
         <h2>What did I learn during the project?</h2>
         <p>
           In the project I used dynamic data and static metadata. Which was
-          rendered by using Gatsby plugins
+          rendered by using Gatsby plugins.
         </p>
         <p>
           During the project I learned about queries, template components,
           markdown files and how to render the information dynamically.
         </p>
-        <div>
-          <em>
-            *This project uses images provided from an out of date Gatsby
-            tutorial.
-          </em>
-        </div>
+
+        <em>
+          *This project uses images provided from an out of date Gatsby
+          tutorial.
+        </em>
       </div>
     </Layout>
   )
